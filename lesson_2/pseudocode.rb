5dab7f0ea1d8ba3ac9fa274527fa8536e0ab862a
Kim Kvarda <kimkvarda@yahoo.com>
2022-08-05 19:06:35 +0000

    in progress

---
+++ lesson_2/pseudocode.rb
@@ -0,0 +1,58 @@
+=begin A method that returns the sum of two integers
+Given there are 2 integers input from user
+Add the two integers
+save the sum of the two integers to a local variable
+return the result
+
+=end 
+
+=begin
+START
+SET integer_1 as user input
+    integer_2 as user input
+    sum = integer_1 + integer_2
+GET user input of two integers
+PRINT sum
+
+END
+=end
+
+=begin A method that takes an array of str and returns a string
+that is all those strings concat together
+Given an array of multiple strings
+Iterate through array
+Take each string and add them together to form one long string
+return this new string value
+=end
+
+=begin
+START
+Given an array of strings
+SET counter = 0
+    concat result variable
+WHILE iterator <= array size
+  result = result variable + current iteration
+PRINT result
+END
+=end
+
+=begin a method that takes array of int and returns new array
+with every other element from original array, start with first
+elementl
+
+Given an array of integers
+Iterate over the array
+return first element then every other element to new array
+print the result
+=end
+
+=begin
+START
+Given an array of integers
+SET counter = 0
+WHILE counter < array size
+  iterate over array elements
+  add to first element, skip every other element
+print the restult
+END
+
